2ed8702b4b2a20b083ae84909c318c3f8f4415ca
The Cheaterman <the.cheaterman@gmail.com>
2023-10-11 18:14:39 +0200

    Add build files to repo

---
+++ dist/index.mjs
@@ -0,0 +1,6 @@
+export { m as main, r as runCommand, h as runMain } from './shared/nuxi.b4d08588.mjs';
+import 'node:util';
+import 'node:path';
+import 'node:process';
+import 'node:tty';
+import 'node:url';
